28ddeea737b11a1f7cf3c01fe4f6982d51e71538
somtam <tim@ocher.co>
2026-05-04 13:15:00 +0700

    confirm box center text

--- widgets/equestionwidget.cpp
+++ widgets/equestionwidget.cpp
@@ -26,7 +26,7 @@ void eQuestionWidget::initialize(const std::string& title,
     textLabel->setText(text);
     textLabel->setTextAlignment(eAlignment::center);
     textLabel->fitContent();
-    textLabel->setX((width - textLabel->width()) / 4);
+    textLabel->setX((width - textLabel->width()) / 2);
     cw->addWidget(textLabel);
     const int tly = titleLabel->y() + titleLabel->height();
     textLabel->setY(tly);
