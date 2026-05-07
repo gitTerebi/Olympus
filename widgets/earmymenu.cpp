1c738f634834d3feb9ff45927e8f2ea32fd9c3a0
somtam <tim@ocher.co>
2026-05-07 12:20:44 +0700

    army tooltips

--- widgets/earmymenu.cpp
+++ widgets/earmymenu.cpp
@@ -1,20 +1,22 @@
 #include "earmymenu.h"
 
+#include "elanguage.h"
 #include "textures/egametextures.h"
 
 #include "eminimap.h"
 #include "ebasicbutton.h"
 #include "eframedwidget.h"
 
-void eArmyMenu::initialize(eGameBoard& b) {
+void eArmyMenu::initialize(eGameBoard &b)
+{
     mBoard = &b;
 
     int iRes;
     int mult;
     iResAndMult(iRes, mult);
 
-    const auto& intrfc = eGameTextures::interface();
-    const auto& coll = intrfc[iRes];
+    const auto &intrfc = eGameTextures::interface();
+    const auto &coll = intrfc[iRes];
     const auto tex = coll.fGameArmyBackground;
     setTexture(tex);
     setPadding(0);
@@ -22,13 +24,13 @@ void eArmyMenu::initialize(eGameBoard& b) {
 
     const auto wid = new eWidget(window());
     wid->setNoPadding();
-    wid->move(mult*11.5, mult*13);
-    wid->resize(mult*71, mult*216);
+    wid->move(mult * 11.5, mult * 13);
+    wid->resize(mult * 71, mult * 216);
     addWidget(wid);
 
     mMiniMap = new eMiniMap(window());
     wid->addWidget(mMiniMap);
-    mMiniMap->resize(wid->width(), mult*56);
+    mMiniMap->resize(wid->width(), mult * 56);
     mMiniMap->setBoard(&b);
 
     const auto status = new eLabel(window());
@@ -36,24 +38,26 @@ void eArmyMenu::initialize(eGameBoard& b) {
     status->setTexture(coll.fArmyStatus);
     status->fitContent();
     wid->addWidget(status);
-    status->resize(wid->width(), mult*23);
-    status->setX(mult*0.75);
-    status->setY(mult*60.75);
+    status->resize(wid->width(), mult * 23);
+    status->setX(mult * 0.75);
+    status->setY(mult * 60.75);
 
-    const int xx = mult*37.75;
-    const int dy = mult*2.5;
-    int y = mult*88.5;
+    const int xx = mult * 37.75;
+    const int dy = mult * 2.5;
+    int y = mult * 88.5;
 
     const auto t1 = &eInterfaceTextures::fGoToCompany;
     const auto cou = new eBasicButton(t1, window());
     wid->addWidget(cou);
     cou->setY(y);
+    cou->setTooltip(eLanguage::zeusText(51, 70)); // Go To Company
 
     const auto t2 = &eInterfaceTextures::fDefensiveTactics;
     const auto dt = new eBasicButton(t2, window());
     wid->addWidget(dt);
     dt->setY(y);
     dt->setX(xx);
+    dt->setTooltip(eLanguage::zeusText(51, 73)); // Defensive Tactics
 
     const int ddy = dy + cou->height();
 
@@ -61,46 +65,52 @@ void eArmyMenu::initialize(eGameBoard& b) {
     const auto rc = new eBasicButton(t3, window());
     wid->addWidget(rc);
     rc->setY(y + ddy);
+    rc->setTooltip(eLanguage::zeusText(68, 141)); // Rotate Company
 
     const auto t4 = &eInterfaceTextures::fOffensiveTactics;
     const auto ot = new eBasicButton(t4, window());
     wid->addWidget(ot);
     ot->setY(y + ddy);
     ot->setX(xx);
+    ot->setTooltip(eLanguage::zeusText(51, 74)); // Offensive Tactics
 
     const auto t5 = &eInterfaceTextures::fGoToBanner;
     mGoToBanner = new eBasicButton(t5, window());
     wid->addWidget(mGoToBanner);
-    mGoToBanner->setY(y + 2*ddy);
-    mGoToBanner->setPressAction([this]() {
+    mGoToBanner->setY(y + 2 * ddy);
+    mGoToBanner->setTooltip(eLanguage::zeusText(51, 25)); // Go To Banner (Muster)
+    mGoToBanner->setPressAction([this]()
+                                {
         mBoard->bannersBackFromHome();
-        setSoldiersHome(false);
-    });
+        setSoldiersHome(false); });
 
     const auto t5_2 = &eInterfaceTextures::fGoHome;
     mGoHome = new eBasicButton(t5_2, window());
     wid->addWidget(mGoHome);
-    mGoHome->setY(y + 2*ddy);
+    mGoHome->setY(y + 2 * ddy);
+    mGoHome->setTooltip(eLanguage::zeusText(51, 26)); // Go Home
     mGoHome->hide();
-    mGoHome->setPressAction([this]() {
+    mGoHome->setPressAction([this]()
+                            {
         mBoard->bannersGoHome();
-        setSoldiersHome(true);
-    });
+        setSoldiersHome(true); });
 
     const auto t6 = &eInterfaceTextures::fSpecialTactics;
     const auto st = new eBasicButton(t6, window());
     wid->addWidget(st);
-    st->setY(y + 2*ddy);
+    st->setY(y + 2 * ddy);
     st->setX(xx);
+    st->setTooltip(eLanguage::zeusText(51, 75)); // Special Tactics
 
     const auto ww = new eFramedWidget(window());
     wid->addWidget(ww);
     ww->setType(eFrameType::inner);
-    ww->setY(y + 3*ddy + mult*2);
-    ww->resize(wid->width(), mult*50);
+    ww->setY(y + 3 * ddy + mult * 2);
+    ww->resize(wid->width(), mult * 50);
 }
 
-void eArmyMenu::setSoldiersHome(const bool h) {
+void eArmyMenu::setSoldiersHome(const bool h)
+{
     mGoToBanner->setVisible(h);
     mGoHome->setVisible(!h);
 }
